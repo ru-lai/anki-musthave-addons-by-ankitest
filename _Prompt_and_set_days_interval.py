5de0d64c745bf73828503ea2c6fca0b5a3a20f0f
Dmitry Mikheev <ankitest@inbox.ru>
2016-05-09 13:02:06 +0500

    Card ease percentage factor has being added
    Also multi-format interval can be set simultaneously

--- _Prompt_and_set_days_interval.py
+++ _Prompt_and_set_days_interval.py
@@ -6,10 +6,30 @@
 #
 from __future__ import division
 from __future__ import unicode_literals
+import os, sys, datetime
 
-import os
+HOTKEY = {      # in mw Main Window (Reviewer)
+    'F2_ctrl'   : ["Ctrl+Alt+Space"],  
+}
+
+SWAP_TAG = False
+SWAP_TAG = datetime.datetime.now().strftime("rescheduled::re-%Y-%m-%d") #-%H:%M:%S")
+#SWAP_TAG = datetime.datetime.now().strftime("re-%y-%m-%d") 
+
+if __name__ == "__main__":
+    print("This is _Swap add-on for the Anki program and it can't be run directly.")
+    print("Please download Anki 2.0 from http://ankisrs.net/")
+    sys.exit()
+else:
+    pass
+
+if sys.version[0] == '2': # Python 3 is utf8 only already.
+  if hasattr(sys,'setdefaultencoding'):
+    sys.setdefaultencoding('utf8')
+
 from aqt import mw
-from aqt.utils import tooltip, getText #, showWarning
+from aqt.utils import tooltip, getText, showInfo
+from anki.utils import intTime
 from anki.hooks import addHook, wrap, runHook
 
 MUSTHAVE_COLOR_ICONS = 'reschedule_icons'
@@ -24,9 +44,21 @@ lang = anki.lang.getLang()
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-HOTKEY = {      # in mw Main Window (Reviewer)
-    'F2_ctrl'   : ["Ctrl+Alt+Space"],  
-}
+def _reschedCards(self, ids, imin, imax, indi=2500):
+    "Put cards in review queue with a new interval in days (min, max)."
+    d = []
+    t = self.today
+    mod = intTime()
+    for id in ids:
+        r = random.randint(imin, imax)
+        d.append(dict(id=id, due=r+t, ivl=max(1, r), mod=mod,
+                      usn=self.col.usn(), fact=indi))
+    self.remFromDyn(ids)
+    self.col.db.executemany("""
+update cards set type=2,queue=2,ivl=:ivl,due=:due,odue=0,
+usn=:usn,mod=:mod,factor=:fact where id=:id""",
+                            d)
+    self.col.log(ids)
 
 # inspired by
 # Date:     January 27, 2016
@@ -39,85 +71,137 @@ import random
 # prompt for new interval, and set it
 def promptNewInterval():
     if mw.state != 'review':
-        tooltip("Задавать дни до следующего просмотра можно только при просмотре карточек!" if lang=='ru' else \
+        tooltip('Задавать дни до следующего просмотра можно только при просмотре карточек!' if lang=='ru' else \
             'Prompt for new interval available only on answer side (BackSide) of card\'s reviewer.')
-    else:
-        suffix = ''
-        days = unicode(mw.reviewer.card.ivl+1)
-        dayString = getText((u"Дней до следующего просмотра карточки (текущий интервал + 1 = %s ):" \
-            if lang=='ru' else \
-            "Number of days until next review (current interval + 1 = %s ):") % (days), default=days)
-        if dayString[1]:
-            dayString0 = dayString[0].strip().lower()
-            dayStringM = False
-            dayStringW = False
-            dayStringI = False
-            if dayString0.endswith('m') or dayString0.endswith(u'м'):
-                dayString0 = dayString0[:-1].strip()
-                dayStringM = True
-            if dayString0.endswith('w') or dayString0.endswith(u'н'):
-                dayString0 = dayString0[:-1].strip()
-                dayStringW = True
-            if dayString0.endswith('d') or dayString0.endswith(u'д'):
-                dayString0 = dayString0[:-1].strip()
-            if len(dayString0)==0:
-                dayString0 = '1'
+        return
 
+    cardEase = None
+    infotip = ''
+    prefix = ''
+    suffix = ''
+    total = 0
+    dayz = float(0)
+    days = unicode(mw.reviewer.card.ivl+1)
+    dayString = getText(('Дней до следующего просмотра карточки (текущий интервал + 1 = %s ):' \
+        if lang=='ru' else \
+        'Number of days until next review (current interval + 1 = %s ):') % (days), default=days)
+
+    stringM = False
+    stringW = False
+    stringD = False
+
+    if dayString[1]:
+      daysList = dayString[0].strip().lower().split()
+      for nextWord in daysList:
+        nextDate = nextWord.strip()
+        if len(nextDate)==0:
+            continue
+
+        dayStringM = False
+        dayStringW = False
+        dayStringD = False
+
+        if nextDate.endswith('m') or nextDate.endswith(u'м'):
+            nextDate = nextDate[:-1].strip()
+            dayStringM = True
+            stringM = True
+        if nextDate.endswith('w') or nextDate.endswith(u'н'):
+            nextDate = nextDate[:-1].strip()
+            dayStringW = True
+            stringW = True
+        if nextDate.endswith('d') or nextDate.endswith(u'д'):
+            nextDate = nextDate[:-1].strip()
+            dayStringD = True
+            stringD = True
+        if len(nextDate)==0:
+            nextDate = '1'
+            dayStringD = True
+            stringD = True
+
+        if nextDate.endswith('%'):
+            nextDate = nextDate[:-1].strip()
             try:
-                days = int(dayString0)
+                cardEase = max(130,abs(int(nextDate)))
+            except ValueError:
+                cardEase = 130
+        else:
+            prefix += nextWord + ' '
+            try:
                 dayz = float(0)
+                days = int(nextDate)
                 if dayStringM:
                     dayz = abs(float(days))
                     days = 0
                 if dayStringW:
-                    dayz = float(0)
                     days = abs(days) * 7
-                if days < 0:
-                    dayStringI = True
             except ValueError:
                 days = mw.reviewer.card.ivl+1
                 try:
-                    dayz = abs(float(dayString0))
+                    dayz = abs(float(nextDate))
                     if 0<dayz and dayz<1:
                         days = int(dayz*10) * 7
-                        dayz = 0
+                        dayz = float(0)
                         dayStringW = True
+                        stringW = True
                     else:
                         days = 0
                         dayStringM = True
+                        stringM = True
                 except ValueError:
                     dayz = float(0)
 
-            if dayz > 0: # 3.1 or 1.2 is monthes
-                suffix = '&plusmn;15'
-                days = int(31*dayz)+random.randrange(-15, 15+1, 1)
-            elif dayStringW: # .2 is two weeks
-                suffix = '&plusmn;3'
-                days = abs(days)+random.randrange(-3, 3+1, 1)
-            elif days > 10:
-                suffix = '&plusmn;1'
-                days = days + random.randrange(-1, 1+1, 1)
-            elif days > 0: # from 1 to 9 setup exact number of day
-                suffix = ''
-                pass # days = days 
-            elif days <= 0:
-                suffix = ''
-                days = mw.reviewer.card.ivl+1
-                #days = days * 60 # num<0 == interval in seconds
+            if dayStringM: 
+                total += int(31*abs(dayz)) 
+            else:
+                total += abs(days) 
 
-            #showWarning(unicode(dayString)+' '+str(dayz)+' '+str(days))
+      mw.checkpoint(_("Reschedule card"))
 
-            mw.checkpoint(_("Reschedule card"))
+      days = total
 
-            mw.col.sched.reschedCards( [mw.reviewer.card.id], days-1 if days>1 else 1, days+1 )
+      if stringD or (not stringD and not stringW and not stringM):
+        if days > 9:
+          suffix = '&plusmn;1'
+          total = days + random.randrange(-1, 1+1, 1)
+        else: # from 1 to 9 setup exact number of day
+          suffix = ''
+          total = days # days = days
+      elif stringW: # .2 is two weeks
+        suffix = '&plusmn;3'
+        total = abs(days) + random.randrange(-3, 3+1, 1)
+      elif stringM: # 3.1 or 1.2 is monthes
+        suffix = '&plusmn;15'
+        total = abs(days) + random.randrange(-15, 15+1, 1)
 
-            days_mod = (days % 10) if ( (days%100) < 11 or (days%100) > 14) else (days % 100)
-            tooltip( (u"Запланирован просмотр через <b>%s</b> %s "+\
+      if cardEase is None:
+        infotip = ""
+        cardEase = 2500
+      else:
+        infotip = ("Лёгкость карточки <b>%s</b>%%<br><br>" if lang=='ru' else \
+          'Card ease <b>%s</b>%%<br><br>') % (cardEase)
+        cardEase *= 10
+
+      if total:
+        #mw.col.sched.reschedCards( [mw.reviewer.card.id], total-1 if total>1 else 1, total+1 )
+        _reschedCards(mw.col.sched, [mw.reviewer.card.id], total-1 if total>1 else 1, total+1, indi=cardEase)
+
+        days_mod = (total % 10) if ( (total%100) < 11 or (total%100) > 14) else (total % 100)
+        tooltip( infotip+("Запланирован просмотр через <b>%s</b> %s "+\
                 ("день" if days_mod==1 else ("дня" if days_mod>=2 and days_mod<=4 else "дней")) if lang=='ru' else \
                 'Rescheduled for review in <b>%s</b> %s days') % \
-                (days, ' (%s%s) '%(dayString[0],suffix) if len(suffix) else '') )
+                (total, ' ( <b style="color:#666;">%s</b> %s ) ' % (prefix.strip(), suffix) if len(suffix) else '') )
 
-            mw.reset()
+        if SWAP_TAG:
+          note = mw.reviewer.card.note()
+          if not note.hasTag(SWAP_TAG):
+            note.addTag(SWAP_TAG)
+            note.flush()  # never forget to flush
+
+      elif cardEase is not None:
+        mw.reviewer.card.factor = cardEase
+        mw.reviewer.card.flush()
+
+      mw.reset()
 
 if True:
     try:
@@ -145,4 +229,3 @@ if True:
     mw.deckBrowser.show = wrap(mw.deckBrowser.show, edit_actions_off)
     mw.overview.show = wrap(mw.overview.show, edit_actions_off)
     mw.reviewer.show = wrap(mw.reviewer.show, edit_actions_on)
-
