400702a46e19345b80b524f2887f9a59fe92a411
Dmitry Mikheev <ankitest@inbox.ru>
2016-05-14 19:11:59 +0500

    one more Steve AW util

---
+++ _Check_media_dont_delete_files.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# ~ Check media dont delete files
+# https://ankiweb.net/shared/info/1051530260
+# License: GNU GPL, version 3 or later
+# Copyright (c) 2016 Dmitry Mikheev, http://finpapa.ucoz.net/
+# 
+# Check media... don't delete files into the basket,
+#  just move them to an outside folder.
+"""
+Copyright: Steve AW <steveawa@gmail.com>
+License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+"""
+from __future__ import unicode_literals
+import os, datetime, shutil
+from anki.lang import _
+from aqt.main import AnkiQt
+from aqt.utils import askUser, tooltip, showWarning, openFolder
+
+__author__ = 'Steve'
+
+NOW = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+
+def deleteUnused(self, unused, diag):
+    base_del_dir = self.pm._ensureExists(
+            os.path.join(self.pm.profileFolder(), "backups"))
+    dest_del_dir = os.path.join(base_del_dir , 'deleted%s' % (NOW))
+    if not askUser(
+        _("Move unused media files to the folder<br><i>%s</i>")%(dest_del_dir)):
+        return
+    if  os.path.exists(dest_del_dir):
+        showWarning('Folder already exists')
+        return
+    os.makedirs(dest_del_dir)
+    mdir = self.col.media.dir()
+    for f in unused:
+        source_path = os.path.join(mdir, f)
+        target_path = os.path.join(dest_del_dir, f)
+        shutil.move(source_path,target_path )
+    tooltip(_("Media files were deleted into<br> %s")%(dest_del_dir))
+    if askUser(_("<b>Open folder</b><br><i>%s</i>")%(dest_del_dir)):
+        openFolder(dest_del_dir)
+    diag.close()
+
+AnkiQt.deleteUnused=deleteUnused
