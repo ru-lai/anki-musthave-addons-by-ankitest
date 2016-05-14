09b5334b3715d27c0af031c2eaaf633a811cc3f5
Dmitry Mikheev <ankitest@inbox.ru>
2016-05-14 23:54:07 +0500

    removing to recycle bin
    available by reply NO

--- _Check_media_dont_delete_files.py
+++ _Check_media_dont_delete_files.py
@@ -6,13 +6,17 @@
 # 
 # Check media... don't delete files into the basket,
 #  just move them to an outside folder.
+# If answer NO then
+#  usual removal into the trash will be done.
 """
 Copyright: Steve AW <steveawa@gmail.com>
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 """
 from __future__ import unicode_literals
 import os, datetime, shutil
+
 from anki.lang import _
+from anki.hooks import wrap
 from aqt.main import AnkiQt
 from aqt.utils import askUser, tooltip, showWarning, openFolder
 
@@ -20,18 +24,19 @@ __author__ = 'Steve'
 
 NOW = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 
-def deleteUnused(self, unused, diag):
-    base_del_dir = self.pm._ensureExists(
-            os.path.join(self.pm.profileFolder(), "backups"))
-    dest_del_dir = os.path.join(base_del_dir , 'deleted%s' % (NOW))
-    if not askUser(
-        _("Move unused media files to the folder<br><i>%s</i>")%(dest_del_dir)):
-        return
+def deleteUnused(self, unused, diag, _old):
+  base_del_dir = self.pm._ensureExists(
+      os.path.join(self.pm.profileFolder(), "backups"))
+  dest_del_dir = os.path.join(base_del_dir , 'deleted%s' % (NOW))
+  mdir = self.col.media.dir()
+  if not askUser(
+      _("Move unused media files to the folder<br><i>%s</i>")%(dest_del_dir)):
+        return _old(self, unused, diag)
+  else:
     if  os.path.exists(dest_del_dir):
         showWarning('Folder already exists')
         return
     os.makedirs(dest_del_dir)
-    mdir = self.col.media.dir()
     for f in unused:
         source_path = os.path.join(mdir, f)
         target_path = os.path.join(dest_del_dir, f)
@@ -40,5 +45,6 @@ def deleteUnused(self, unused, diag):
     if askUser(_("<b>Open folder</b><br><i>%s</i>")%(dest_del_dir)):
         openFolder(dest_del_dir)
     diag.close()
+    return True
 
-AnkiQt.deleteUnused=deleteUnused
+AnkiQt.deleteUnused = wrap(AnkiQt.deleteUnused, deleteUnused, 'around')
