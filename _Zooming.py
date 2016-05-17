43ec1365a701b165fe2c3cd1483302ddfe930407
Dmitry Mikheev <ankitest@inbox.ru>
2016-05-17 16:35:11 +0500

    Menu item to switch Zoom images
    Again and again :-)
    And quotes 2 apostrophes.

--- _Zooming.py
+++ _Zooming.py
@@ -10,9 +10,9 @@ from __future__ import division
 from __future__ import unicode_literals
 import os, sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("This is _Zooming add-on for the Anki program and it can't be run directly.")
-    print("Please download Anki 2.0 from http://ankisrs.net/")
+    print('Please download Anki 2.0 from http://ankisrs.net/')
     sys.exit()
 else:
     pass
@@ -35,15 +35,15 @@ reviewer_standard_zoom = 1.4
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-FONT = False
+FONT = False # Use font as it comes from Anki
 FONTSIZE = 0 # Use default font size
 
-FONT = "Calibri"
+FONT = 'Calibri' # Use custom typeface
 FONTSIZE = 16 #12 #18 #20 #24 
 
 from aqt import mw
 from aqt.qt import *
-from aqt.utils import showText, showCritical
+from aqt.utils import showText, showWarning, showCritical
 from anki.hooks import addHook, wrap, runHook
 
 def changeFont():
@@ -72,11 +72,11 @@ except:
     MUSTHAVE_COLOR_ICONS = ''
 
 HOTKEY = {      # in mw Main Window (deckBrowser, Overview, Reviewer)
-    'zoom_info'  : ["Alt+0", '', "", ''' ''', """ """], 
-    'zoom_in'    : ["Ctrl++", '', "", ''' ''', """ """], 
-    'zoom_out'   : ["Ctrl+-", '', "", ''' ''', """ """], 
-    'zoom_reset' : ["Ctrl+0", '', "", ''' ''', """ """], 
-    'zoom_init'  : ["Ctrl+Alt+0", '', "", ''' ''', """ """], 
+    'zoom_info'  : ['Alt+0', '', '', ''' ''', """ """], 
+    'zoom_in'    : ['Ctrl++', '', '', ''' ''', """ """], 
+    'zoom_out'   : ['Ctrl+-', '', '', ''' ''', """ """], 
+    'zoom_reset' : ['Ctrl+0', '', '', ''' ''', """ """], 
+    'zoom_init'  : ['Ctrl+Alt+0', '', '', ''' ''', """ """], 
     }
 
 ##################################################################
@@ -97,7 +97,7 @@ reviewer_current_zoom = reviewer_standard_zoom
 # Based in part on code by Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-"""Add-on for Anki 2 to zoom in or out."""
+'''Add-on for Anki 2 to zoom in or out.'''
 
 def reset_current_zoom():
     global deck_browser_current_zoom, overview_current_zoom, reviewer_current_zoom
@@ -204,48 +204,74 @@ def current_reset_zoom(state=None, *args):
         mw.web.setTextSizeMultiplier(current_zoom)
 
 def zoom_info():
-    showText("<big>deck_browser_standard_zoom = <b>"+str(deck_browser_current_zoom)+"</b><br>\n"+\
-        "overview_standard_zoom = <b>"+str(overview_current_zoom)+"</b><br>\n"+\
-        "reviewer_standard_zoom = <b>"+str(reviewer_current_zoom)+"</b></big>\n",\
-        type="HTML")
+    showText('<table><tbody>\n'+\
+        '<tr><td align=right><big>deck_browser_standard_zoom = </big>'+\
+        '</td><td><big><b>'+str(deck_browser_current_zoom)+'</b></big></td></tr>\n'+\
+        '<tr><td align=right><big>overview_standard_zoom = </big>'+\
+        '</td><td><big><b>'+str(overview_current_zoom)+'</b></td></tr>\n'+\
+        '<tr><td align=right><big>reviewer_standard_zoom = </big>'+\
+        '</td><td><big><b>'+str(reviewer_current_zoom)+'</b><br></big></td></tr>\n'+\
+
+        '<tr><td align=right>ZOOM_IMAGES = </td><td><b>' +unicode(ZOOM_IMAGES) +'</b></td></tr>\n'+\
+        '<tr><td align=right>textSizeMultiplier = </td><td><b>' +str(mw.web.textSizeMultiplier()) +'</b></td></tr>'+\
+        '<tr><td align=right>zoomFactor = </td><td><b>' +str(mw.web.zoomFactor()) +'</b></td></tr>'+\
+        '</tbody></table>',type='HTML')
+
+def zoom_images(act):
+    global ZOOM_IMAGES
+    ZOOM_IMAGES = act.isChecked()
+    if ZOOM_IMAGES:
+        showWarning('Чтобы изменения вступили в силу —<br> пожалуйста, <b>перезапустите Anki</b>' if lang=='ru' \
+            else 'Please, <b>restart Anki</b><br> to apply changes.')
+    current_reset_zoom()
 
 try:
     mw.addon_view_menu
 except AttributeError:
-    mw.addon_view_menu = QMenu(_("&Вид") if lang == 'ru' else _("&View"), mw)
+    mw.addon_view_menu = QMenu(_('&Вид') if lang == 'ru' else _('&View'), mw)
     mw.form.menubar.insertMenu(
         mw.form.menuTools.menuAction(), mw.addon_view_menu)
 
+zoom_images_action = None # global 
+
 def zoom_setup_menu():
-    # Set up the zoom menu.
-    mw.zoom_submenu = QMenu('Мас&штаб' if lang == 'ru' else _("&Zoom"), mw)
+    global zoom_images_action
+
+    mw.zoom_submenu = QMenu('Мас&штаб' if lang == 'ru' else _('&Zoom'), mw)
     mw.zoom_submenu.setIcon(QIcon(os.path.join(MUSTHAVE_COLOR_ICONS, 'zoom.png')))
 
-    zoom_info_action = QAction('Масштаб &показать' if lang == 'ru' else _('Zoom &Info'), mw)
+    zoom_info_action = QAction('Масштаб &показать' if lang == 'ru' else _('Zoom In&fo'), mw)
     zoom_info_action.setShortcut(QKeySequence(HOTKEY['zoom_info'][0])) # Ctrl+Shift+0 doesn't work on NumPad
-    mw.connect(zoom_info_action, SIGNAL("triggered()"), zoom_info)
+    mw.connect(zoom_info_action, SIGNAL('triggered()'), zoom_info)
+
+    zoom_images_action = QAction('Масштаб &картинок менять' if lang == 'ru' else _('&Zoom Images'), mw)
+    #zoom_images_action.setShortcut(QKeySequence(HOTKEY['zoom_images'][0])) 
+    zoom_images_action.setCheckable(True) 
+    zoom_images_action.setChecked(ZOOM_IMAGES) 
+    mw.connect(zoom_images_action, SIGNAL('triggered()'), lambda AKT=zoom_images_action: zoom_images(AKT) )
 
     zoom_in_action = QAction('Масштаб у&величить' if lang == 'ru' else _('Zoom &In'), mw)
     zoom_in_action.setShortcut(QKeySequence(HOTKEY['zoom_in'][0]))
     zoom_in_action.setIcon(QIcon(os.path.join(MUSTHAVE_COLOR_ICONS, 'zoom_in.png')))
-    mw.connect(zoom_in_action, SIGNAL("triggered()"), zoom_in)
+    mw.connect(zoom_in_action, SIGNAL('triggered()'), zoom_in)
 
     zoom_out_action = QAction('Масштаб у&меньшить' if lang == 'ru' else _('Zoom &Out'), mw)
     zoom_out_action.setShortcut(QKeySequence(HOTKEY['zoom_out'][0]))
     zoom_out_action.setIcon(QIcon(os.path.join(MUSTHAVE_COLOR_ICONS, 'zoom_out.png')))
-    mw.connect(zoom_out_action, SIGNAL("triggered()"), zoom_out)
+    mw.connect(zoom_out_action, SIGNAL('triggered()'), zoom_out)
 
     reset_zoom_action = QAction('Масштаб на&чальный ( ='+str(deck_browser_standard_zoom)+' ='+str(overview_standard_zoom)+' ='+str(reviewer_standard_zoom)+' )' if lang == 'ru' else _('&Reset Initial ')+'( ='+str(deck_browser_standard_zoom)+' ='+str(overview_standard_zoom)+' ='+str(reviewer_standard_zoom)+' )', mw)
     reset_zoom_action.setShortcut(QKeySequence(HOTKEY['zoom_reset'][0])) # Shift+0 does not work on NumPad
-    mw.connect(reset_zoom_action, SIGNAL("triggered()"), zoom_reset)
+    mw.connect(reset_zoom_action, SIGNAL('triggered()'), zoom_reset)
 
-    reset_zoom_init_action = QAction('Мас&штаб 1:1 100% ( =1.0 =1.0 =1.0 )' if lang == 'ru' else _('&Reset 1:1 100% ( =1.0 =1.0 =1.0 )'), mw)
+    reset_zoom_init_action = QAction('Мас&штаб 1:1 100% ( =1.0 =1.0 =1.0 )' if lang == 'ru' else _('Reset &1:1 100% ( =1.0 =1.0 =1.0 )'), mw)
     reset_zoom_init_action.setShortcut(QKeySequence(HOTKEY['zoom_init'][0]))
-    mw.connect(reset_zoom_init_action, SIGNAL("triggered()"), zoom_init)
+    mw.connect(reset_zoom_init_action, SIGNAL('triggered()'), zoom_init)
 
     if hasattr(mw,'addon_view_menu'):
         mw.addon_view_menu.addMenu(mw.zoom_submenu)
         mw.zoom_submenu.addAction(zoom_info_action)
+        mw.zoom_submenu.addAction(zoom_images_action)
         mw.zoom_submenu.addSeparator()
         mw.zoom_submenu.addAction(zoom_in_action)
         mw.zoom_submenu.addAction(zoom_out_action)
@@ -274,13 +300,14 @@ def handle_wheel_event(event):
         original_mw_web_wheelEvent(event)
 
 def run_move_to_state_hook(state, *args):
-    """Run a hook whenever we have changed the state."""
-    runHook("movedToState", state)
+    '''Run a hook whenever we have changed the state.'''
+    runHook('movedToState', state)
 
 mw.moveToState = wrap(mw.moveToState, run_move_to_state_hook)
-addHook("movedToState", current_reset_zoom)
+addHook('movedToState', current_reset_zoom)
 original_mw_web_wheelEvent = mw.web.wheelEvent
 mw.web.wheelEvent = handle_wheel_event
+
 zoom_setup_menu()
 
 ##
@@ -289,9 +316,11 @@ def save_toolbarz_visible():
     mw.pm.profile['ctb_deck_browser_zoom'] = deck_browser_current_zoom
     mw.pm.profile['ctb_overview_zoom'] = overview_current_zoom
     mw.pm.profile['ctb_reviewer_zoom'] = reviewer_current_zoom
+    mw.pm.profile['ctb_images_zoom'] = ZOOM_IMAGES
 
 def load_toolbarz_visible():
     global deck_browser_current_zoom, overview_current_zoom, reviewer_current_zoom
+    global zoom_images_action, ZOOM_IMAGES
 
     try:
         key_value = mw.pm.profile['ctb_deck_browser_zoom']
@@ -314,13 +343,20 @@ def load_toolbarz_visible():
         pass
     #   reviewer_current_zoom = reviewer_standard_zoom
 
+    try:
+        key_value = mw.pm.profile['ctb_images_zoom']
+        ZOOM_IMAGES = key_value
+    except KeyError:
+        pass
+
+    zoom_images_action.setChecked(ZOOM_IMAGES)
     current_reset_zoom()
 
-addHook("unloadProfile", save_toolbarz_visible)
-addHook("profileLoaded", load_toolbarz_visible)
+addHook('unloadProfile', save_toolbarz_visible)
+addHook('profileLoaded', load_toolbarz_visible)
 
 ##
 
-if os.path.exists(os.path.join(mw.pm.addonFolder(), "Zoom.py")):
+if os.path.exists(os.path.join(mw.pm.addonFolder(), 'Zoom.py')):
    showCritical( 'Найдено старое дополнение <b>Zoom</b> <br><br> Оно несовместимо с новым дополнением <i>_Zooming</i><br> &nbsp; Для нормальной работы:<br> &nbsp;  &nbsp; - удалите старое дополнение<br>  &nbsp; &nbsp; &nbsp; - перезапустите Anki.' if lang == 'ru' else 'The old version of <b>Zoom</b> add-on <br><br> is incompatible with new edition of <i>_Zooming</i>. <br> Please, remove old file and restart Anki.' )
 
