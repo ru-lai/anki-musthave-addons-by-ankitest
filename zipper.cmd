da57f8092ea9b8205e8ec57840ef465caa4224f2
Dmitry Mikheev <ankitest@inbox.ru>
2016-05-18 18:52:01 +0500

    Editor fontsize F4 icons

--- zipper.cmd
+++ zipper.cmd
@@ -1,3 +1,4 @@
+"c:\Program Files\7-Zip\7z.exe" a -tzip _Editor_fontsize.zip _Editor_fontsize.py f4edit_icons\*
 "c:\Program Files\7-Zip\7z.exe" a -tzip _F4_Edit.zip _F4_Edit.py f4edit_icons\*
 "c:\Program Files\7-Zip\7z.exe" a -tzip _Flip-flop.zip _Flip-flop.py flipflop_icons\*
 "c:\Program Files\7-Zip\7z.exe" a -tzip _Prompt_and_set_days.zip _Prompt_and_set_days_interval_and_card_ease.py reschedule_icons\*
