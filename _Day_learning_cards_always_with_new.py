dcb29086eaf5139a8cdb0cc307f1b76f44336487
Dmitry Mikheev <ankitest@inbox.ru>
2016-07-14 00:44:55 +0500

    if new cards before option is activated
    then new cards are going earlier than learning cards

---
+++ _Day_learning_cards_always_with_new.py
@@ -0,0 +1,92 @@
+﻿# -*- mode: Python ; coding: utf-8 -*-
+# • day learning cards always with new
+# https://ankiweb.net/shared/info/1331545236
+# https://github.com/ankitest/anki-musthave-addons-by-ankitest
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# Copyright (c) 2016 Dmitry Mikheev, http://finpapa.ucoz.net/
+
+# Now it is the part of Must Have add-on:
+# https://ankiweb.net/shared/info/67643234
+
+# This is a simple monkey patch add-on that inserts day learning cards
+# (learning cards with intervals that crossed the day turnover)
+# always before new cards without depending due reviews.
+
+# By default Anki do so:
+#  learning; new if before; due; day learning; new if after
+# With this add-on card will be displayed in the following order:
+#  learning; (day learning; new) if before; due; (day learning; new) if after
+
+# Normally these cards go after due, but I want them to go before new.
+
+# If Tools -> Preferences... -> Basic -> Show new cards before reviews
+#    learning; day learning; new; due
+# If Tools -> Preferences... -> Basic -> Show new cards after reviews
+#    learning; due; day learning; new
+
+# inspired by Anki user rjgoif
+# https://ankiweb.net/shared/info/1810271825
+# put ALL due "learning" cards first ×
+# ####################################################################
+# That is a simple add-on that inserts the daily-learning cards, i.e.
+# cards in the learning queue with intervals that crossed the day turnover,
+# before starting other reviews (new cards, review cards). \
+# Normally these cards go last, but I want them to go first.
+# ####################################################################
+
+# after https://anki.tenderapp.com/discussions/ankidesktop
+# /18936-function-not-working-showing-new-cards-first
+
+# if Show new cards before reviews option is selected then
+# New cards are going before old ones, learning and reviews.
+
+#
+# No support. Use it AS IS on your own risk.
+import anki.sched
+
+
+def _getCardReordered(self):
+    'Return the next due card id, or None.'
+
+    # new first, or time for one?
+    if self._timeForNewCard():
+
+        c = self._getNewCard()
+        if c:
+            return c
+
+        # learning card due?
+        c = self._getLrnCard()
+        if c:
+            return c
+
+        # day learning card due?
+        c = self._getLrnDayCard()
+        if c:
+            return c
+
+    else:
+        # learning card due?
+        c = self._getLrnCard()
+        if c:
+            return c
+
+    # card due for review?
+    c = self._getRevCard()
+    if c:
+        return c
+
+    # day learning card due?
+    c = self._getLrnDayCard()
+    if c:
+        return c
+
+    # new cards left?
+    c = self._getNewCard()
+    if c:
+        return c
+
+    # collapse or finish
+    return self._getLrnCard(collapse=True)
+
+anki.sched.Scheduler._getCard = _getCardReordered
