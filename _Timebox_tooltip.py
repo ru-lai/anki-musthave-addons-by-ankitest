f8943f8cedd0be96863ee35cd99367477835bb54
Dmitry Mikheev <ankitest@inbox.ru>
2017-02-21 19:35:55 +0500

    It does not conflict with Anki Habitica now.

--- _Timebox_tooltip.py
+++ _Timebox_tooltip.py
@@ -1,24 +1,14 @@
 # -*- coding: utf-8 -*-
-# • Timebox tooltip
+# ' Timebox tooltip
 # https://ankiweb.net/shared/info/2014169675
 # https://github.com/ankitest/anki-musthave-addons-by-ankitest
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Copyright (c) 2016 Dmitry Mikheev, http://finpapa.ucoz.net/
+# Copyright (c) 2016-2017 Dmitry Mikheev, http://finpapa.ucoz.net/
 #
 # It puts the stats when you finish a timebox in a tooltip message
 #  that goes away after a few seconds. 
 #
 # No support. Use it AS IS on your own risk.
-"""
-    def timeboxReached(self):
-        "Return (elapsedTime, reps) if timebox reached, or False."
-        if not self.conf['timeLim']:
-            # timeboxing disabled
-            return False
-        elapsed = time.time() - self._startTime
-        if elapsed > self.conf['timeLim']:
-            return (self.conf['timeLim'], self.sched.reps - self._startReps)
-"""
 
 from anki.hooks import wrap
 from anki.sound import playFromText, clearAudioQueue, play
@@ -26,11 +16,10 @@ from aqt import mw
 from aqt.reviewer import Reviewer
 from aqt.utils import tooltip
 
-# anki/collection.py
-# aqt/reviewer.py Monkey Patch
+# anki/collection.py: .timeboxReached() .startTimebox()
+# aqt/reviewer.py: .nextCard 
 
-
-def maNextCard(self, _old):
+def maNextCard(self):
     elapsed = self.mw.col.timeboxReached()
     if elapsed:
         part1 = ngettext('%d card studied in',
@@ -39,27 +28,5 @@ def maNextCard(self, _old):
         part2 = ngettext('%s minute.', '%s minutes.', mins) % mins
         tooltip('%s %s' % (part1, part2))
         self.mw.col.startTimebox()
-    if self.cardQueue:
-        # undone/edited cards to show
-        c = self.cardQueue.pop()
-        c.startTimer()
-        self.hadCardQueue = True
-    else:
-        if self.hadCardQueue:
-            # the undone/edited cards may be sitting in the regular queue;
-            # need to reset
-            self.mw.col.reset()
-            self.hadCardQueue = False
-        c = self.mw.col.sched.getCard()
-    self.card = c
-    clearAudioQueue()
-    if not c:
-        self.mw.moveToState('overview')
-        return
-    if self._reps is None or self._reps % 100 == 0:
-        # we recycle the webview periodically so webkit can free memory
-        self._initWeb()
-    else:
-        self._showQuestion()
 
-Reviewer.nextCard = wrap(Reviewer.nextCard, maNextCard, 'around')
+Reviewer.nextCard = wrap(Reviewer.nextCard, maNextCard, 'before')
