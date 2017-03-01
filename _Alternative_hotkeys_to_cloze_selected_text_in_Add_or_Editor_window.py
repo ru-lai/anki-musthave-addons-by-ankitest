63bdec50f8191a01ed572c8444f8e49e56d2b737
Dmitry Mikheev <ankitest@inbox.ru>
2017-03-01 13:59:19 +0500

    F1 same F2 next cloze
    without Alt at all

--- _Alternative_hotkeys_to_cloze_selected_text_in_Add_or_Editor_window.py
+++ _Alternative_hotkeys_to_cloze_selected_text_in_Add_or_Editor_window.py
@@ -16,12 +16,38 @@
 #
 # No support. Use it AS IS on your own risk.
 
+from aqt.utils import tooltip, showInfo
 from anki.hooks import wrap
 import aqt.editor
 from aqt.editor import *
 
 CtrlSpace = 'Ctrl+Space'
 CtrlAltSpace = 'Ctrl+Alt+Space'
+
+
+def onAltCloze(self, delta):
+    # check that the model is set up for cloze deletion
+    if not re.search('{{(.*:)*cloze:',self.note.model()['tmpls'][0]['qfmt']):
+        if self.addMode:
+            tooltip(_("Warning, cloze deletions will not work until "
+            "you switch the type at the top to Cloze."))
+        else:
+            showInfo(_("""\
+To make a cloze deletion on an existing note, you need to change it \
+to a cloze type first, via Edit>Change Note Type."""))
+            return
+    # find the highest existing cloze
+    highest = 0
+    for name, val in self.note.items():
+        m = re.findall("\{\{c(\d+)::", val)
+        if m:
+            highest = max(highest, sorted([int(x) for x in m])[-1])
+    # reuse last?
+    #if not self.mw.app.keyboardModifiers() & Qt.AltModifier:
+    highest += delta
+    # must start at 1
+    highest = max(1, highest)
+    self.web.eval("wrap('{{c%d::', '}}');" % highest)
 
 
 def setupButtonz(self):
@@ -31,6 +57,18 @@ def setupButtonz(self):
 
     s = QShortcut(
         QKeySequence(CtrlAltSpace), self.parentWindow)
+    s.connect(s, SIGNAL('activated()'), self.onCloze)
+
+    s = QShortcut(
+        QKeySequence('F1'), self.parentWindow)
+    s.connect(s, SIGNAL('activated()'), lambda: onAltCloze(self, 0))
+
+    s = QShortcut(
+        QKeySequence('F2'), self.parentWindow)
+    s.connect(s, SIGNAL('activated()'), self.onCloze)
+
+    s = QShortcut(
+        QKeySequence('Alt+F2'), self.parentWindow)
     s.connect(s, SIGNAL('activated()'), self.onCloze)
 
 aqt.editor.Editor.setupButtons = wrap(
