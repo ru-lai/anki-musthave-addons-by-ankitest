d02e532e267911ea18e6d3dfcd75e1bf22e65d58
Dmitry Mikheev <ankitest@inbox.ru>
2017-04-21 23:26:16 +0500

    2 menu items 4 top & bottom widgets

--- _View_HTML_source_with_JavaScript_and_CSS_styles.py
+++ _View_HTML_source_with_JavaScript_and_CSS_styles.py
@@ -21,14 +21,14 @@ from __future__ import unicode_literals
 import os
 import sys
 
+import anki
+import aqt
+
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-from aqt import mw
-from aqt.utils import showText
-
 # Get language class
-import anki.lang
+#import anki.lang
 lang = anki.lang.getLang()
 
 MSG = {
@@ -38,6 +38,8 @@ MSG = {
         'Cards': _('&Cards'),
         'no_jQuery': _('View Source code &Body'),
         'view_source': _('&View Source code'),
+        'toolbarWeb': _('View toolbar Source'),
+        'bottomWeb': _('View bottomWeb Source'),
         },
     'ru': {
         'later': 'позже',
@@ -45,6 +47,8 @@ MSG = {
         'Cards': '&Карточки',
         'no_jQuery': 'Показать Ис&ходник HTML Body',
         'view_source': 'Показать И&сходник HTML',
+        'toolbarWeb': 'Показать Верхний Исходник',
+        'bottomWeb': 'Показать Нижний Исходник',
         },
     }
 
@@ -55,14 +59,14 @@ except KeyError:
 
 try:
     MUSTHAVE_COLOR_ICONS = os.path.join(
-        mw.pm.addonFolder(), 'handbook')
+        aqt.mw.pm.addonFolder(), 'handbook')
 except:
     MUSTHAVE_COLOR_ICONS = ''
 
 # 'Показать Ис&ходник HTML Body' if lang == 'ru' else 'View Source code &Body'
 # 'Показать И&сходник HTML' if lang == 'ru' else '&View Source code'
 
-HOTKEY = {      # in mw Main Window (deckBrowser, Overview, Reviewer)
+HOTKEY = {      # in aqt.mw Main Window (deckBrowser, Overview, Reviewer)
     'HTML_source': 'Ctrl+F3',
     'Body_source': 'Alt+F3',
 }
@@ -71,64 +75,85 @@ __addon__ = "'" + __name__.replace('_', ' ')
 __version__ = "2.0.44a"
 
 try:
-    mw.addon_cards_menu
+    aqt.mw.addon_cards_menu
 except AttributeError:
-    mw.addon_cards_menu = QMenu(MSG[lang]['Cards'], mw)
-    mw.form.menubar.insertMenu(
-        mw.form.menuTools.menuAction(), mw.addon_cards_menu)
+    aqt.mw.addon_cards_menu = QMenu(MSG[lang]['Cards'], aqt.mw)
+    aqt.mw.form.menubar.insertMenu(
+        aqt.mw.form.menuTools.menuAction(), aqt.mw.addon_cards_menu)
 
 
 def _getSourceHTML():
     """To look at sourcne HTML+CSS code."""
-    html = mw.web.page().mainFrame().evaluateJavaScript("""
+    html = aqt.mw.web.page().mainFrame().evaluateJavaScript("""
         (function(){
              return document.documentElement.outerHTML
          }())
     """)
-    showText(html)
+    aqt.utils.showText(html)
 
 
-def _getSourceBody():
+def _getSourceBody(mWeb):
     """To look at sourcne HTML+CSS code."""
     html = '<html class="' +\
-        unicode(mw.web.page().mainFrame().evaluateJavaScript("""
+        unicode(mWeb.page().mainFrame().evaluateJavaScript("""
         (function(){
              return document.documentElement.className
          }())
     """)) + '">\n<head>\n' +\
         unicode(
-            mw.web.page().mainFrame().evaluateJavaScript(
+            mWeb.page().mainFrame().evaluateJavaScript(
                 """
         (function(){
              return document.getElementsByTagName('head')[0]""" +
                 """.getElementsByTagName('style')[0].outerHTML
          }())
     """)) + '\n</head>\n' +\
-        unicode(mw.web.page().mainFrame().evaluateJavaScript("""
+        unicode(mWeb.page().mainFrame().evaluateJavaScript("""
         (function(){
              return document.body.outerHTML
          }())
     """)) + '\n</html>\n'
-    showText(html)
+    aqt.utils.showText(html)
 
-get_Body_Source_action = QAction(mw)
+get_Body_Source_action = QAction(aqt.mw)
 get_Body_Source_action.setText(MSG[lang]['no_jQuery'])
 get_Body_Source_action.setShortcut(
     QKeySequence(HOTKEY['Body_source']))
 get_Body_Source_action.setIcon(
     QIcon(os.path.join(MUSTHAVE_COLOR_ICONS, 'html.png')))
-mw.connect(get_Body_Source_action, SIGNAL('triggered()'), _getSourceBody)
+aqt.mw.connect(
+    get_Body_Source_action, SIGNAL('triggered()'), 
+    lambda: _getSourceBody(aqt.mw.web))
 
-get_HTML_Source_action = QAction(mw)
+get_HTML_Source_action = QAction(aqt.mw)
 get_HTML_Source_action.setText(MSG[lang]['view_source'])
 get_HTML_Source_action.setShortcut(
     QKeySequence(HOTKEY['HTML_source']))
 get_HTML_Source_action.setIcon(
     QIcon(os.path.join(MUSTHAVE_COLOR_ICONS, 'html5.png')))
-mw.connect(get_HTML_Source_action, SIGNAL('triggered()'), _getSourceHTML)
+aqt.mw.connect(
+    get_HTML_Source_action, SIGNAL('triggered()'), _getSourceHTML)
 
-if hasattr(mw, 'addon_cards_menu'):
-    mw.addon_cards_menu.addSeparator()
-    mw.addon_cards_menu.addAction(get_Body_Source_action)
-    mw.addon_cards_menu.addAction(get_HTML_Source_action)
-    mw.addon_cards_menu.addSeparator()
+get_Top_Source_action = QAction(aqt.mw)
+get_Top_Source_action.setText(MSG[lang]['toolbarWeb'])
+aqt.mw.connect(
+    get_Top_Source_action, SIGNAL('triggered()'), 
+    lambda: _getSourceBody(aqt.mw.toolbar.web))
+
+get_Bottom_Source_action = QAction(aqt.mw)
+get_Bottom_Source_action.setText(MSG[lang]['bottomWeb'])
+aqt.mw.connect(
+    get_Bottom_Source_action, SIGNAL('triggered()'), 
+    lambda: _getSourceBody(aqt.mw.bottomWeb))
+
+# -- these work perfectly well
+# aqt.mw.toolbar.web.hide()
+# aqt.mw.toolbar.web.setFixedHeight(50)
+
+if hasattr(aqt.mw, 'addon_cards_menu'):
+    aqt.mw.addon_cards_menu.addSeparator()
+    aqt.mw.addon_cards_menu.addAction(get_Top_Source_action)
+    aqt.mw.addon_cards_menu.addAction(get_Body_Source_action)
+    aqt.mw.addon_cards_menu.addAction(get_HTML_Source_action)
+    aqt.mw.addon_cards_menu.addAction(get_Bottom_Source_action)
+    aqt.mw.addon_cards_menu.addSeparator()
