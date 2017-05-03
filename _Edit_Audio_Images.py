3276497cb8976a680553902ff8a6f628459df8f8
Dmitry Mikheev <ankitest@inbox.ru>
2017-05-03 16:15:00 +0500

    Auto Reload Images and Audios
    by Refresh Media References

--- _Edit_Audio_Images.py
+++ _Edit_Audio_Images.py
@@ -27,14 +27,12 @@ import os
 import sys
 import re
 
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+# from PyQt4.QtGui import *
+# from PyQt4.QtCore import *
 
-from anki.hooks import addHook, wrap, runHook
-from aqt import mw
-from aqt.editor import Editor  # the editor when you click 'Add' in Anki
-from aqt.utils import tooltip, showInfo, showWarning, showCritical, showText
-from anki.utils import isWin, isMac
+import anki
+import aqt
+from aqt.qt import *
 
 # Get language class
 import anki.lang
@@ -75,7 +73,7 @@ except KeyError:
 # 'Правка &Аудио из полей' if lang == 'ru' else
 #   _('Edit &Audio from fields')
 
-HOTKEY = {      # in mw Main Window (deckBrowser, Overview, Reviewer)
+HOTKEY = {      # in aqt.mw Main Window (deckBrowser, Overview, Reviewer)
     'edit':  'F10',
     'image': 'Ctrl+F10',
     'audio': 'Shift+F10',
@@ -122,7 +120,7 @@ def JustDoItBy(note, curFld, audioPic):
         flds = note.fields
     else:
         flds = [note.fields[curFld]]
-    pathToCollection = mw.col.media.dir() + "/"
+    pathToCollection = aqt.mw.col.media.dir() + "/"
 
     if audioPic == 0 or audioPic == 1:
         for fld in flds:
@@ -130,8 +128,8 @@ def JustDoItBy(note, curFld, audioPic):
             #   re.search(r'''\<img src=["|'](.*?)["|']''',fld) # '''
             next_pictures = re.findall(r'\<img src="(.*?)"', fld)
             for next_picture in next_pictures:
-                if next_picture and isWin:
-                    found = os.path.join(mw.col.media.dir(),
+                if next_picture and anki.utils.isWin:
+                    found = os.path.join(aqt.mw.col.media.dir(),
                                          next_picture)  # .group(1))
                     if os.path.exists(found):
                         if Windows_IMG:
@@ -144,7 +142,7 @@ def JustDoItBy(note, curFld, audioPic):
                                     os.startfile(found)
                                 except:
                                     pass
-                if next_picture and isMac:
+                if next_picture and anki.utils.isMac:
                     fullPath = os.path.join(pathToCollection, found)
                     # need to escape spaces
                     fullPath = re.sub(" ", "\ ", fullPath)
@@ -155,8 +153,8 @@ def JustDoItBy(note, curFld, audioPic):
             # next_sound = re.search(r'\[sound:(.*?)\]',fld)
             next_sounds = re.findall(r'\[sound:(.*?)\]', fld)
             for next_sound in next_sounds:
-                if next_sound and isWin:
-                    found = os.path.join(mw.col.media.dir(),
+                if next_sound and anki.utils.isWin:
+                    found = os.path.join(aqt.mw.col.media.dir(),
                                          next_sound)  # .group(1))
                     if os.path.exists(found):
                         if Windows_SND:
@@ -171,76 +169,76 @@ def JustDoItBy(note, curFld, audioPic):
                                     os.startfile(found)
                                 except:
                                     pass
-                if next_sound and isMac:
+                if next_sound and anki.utils.isMac:
                     fullPath = os.path.join(pathToCollection, next_sound)
                     fullPath = re.sub(" ", "\ ", fullPath)
                     os.system(macOS_SND + " " + fullPath)
 
 
 def JustDoItByYourself():
-    rst = mw.reviewer.state
-    NB = mw.reviewer.card.note()
+    rst = aqt.mw.reviewer.state
+    NB = aqt.mw.reviewer.card.note()
     JustDoItBy(NB, -1, 0)
-    # mw.reset()  # refresh gui
+    # aqt.mw.reset()  # refresh gui
     # if rst == 'answer':
-    #     mw.reviewer._showAnswerHack()  # ._showAnswer() #
-    # mw.requireReset()
+    #     aqt.mw.reviewer._showAnswerHack()  # ._showAnswer() #
+    aqt.mw.requireReset()
 
 
 def JustDoItByPictures():
-    rst = mw.reviewer.state
-    NB = mw.reviewer.card.note()
+    rst = aqt.mw.reviewer.state
+    NB = aqt.mw.reviewer.card.note()
     JustDoItBy(NB, -1, 1)
-    # mw.reset()  # refresh gui
+    # aqt.mw.reset()  # refresh gui
     # if rst == 'answer':
-    #     mw.reviewer._showAnswerHack()  # ._showAnswer() #
-    # mw.requireReset()
+    #     aqt.mw.reviewer._showAnswerHack()  # ._showAnswer() #
+    aqt.mw.requireReset()
 
 
 def JustDoItBySounds():
-    rst = mw.reviewer.state
-    NB = mw.reviewer.card.note()
+    rst = aqt.mw.reviewer.state
+    NB = aqt.mw.reviewer.card.note()
     JustDoItBy(NB, -1, 2)
-    # mw.reset()  # refresh gui
+    # aqt.mw.reset()  # refresh gui
     # if rst == 'answer':
-    #     mw.reviewer._showAnswerHack()  # ._showAnswer() #
-    # mw.requireReset()
+    #     aqt.mw.reviewer._showAnswerHack()  # ._showAnswer() #
+    aqt.mw.requireReset()
 
 
 def TryItByYourself(edit):
     ecf = edit.currentField
     JustDoItBy(edit.note, ecf, 0)
-    mw.reset()  # refresh gui
+    aqt.mw.reset()  # refresh gui
     # focus field so it's saved
     edit.web.setFocus()
     edit.web.eval('focusField(%d);' % ecf)
 
 ##
 
-edit_action = QAction(MSG[lang]['Edit Audio Images'], mw)
+edit_action = QAction(MSG[lang]['Edit Audio Images'], aqt.mw)
 edit_action.setShortcut(QKeySequence(HOTKEY['edit']))
 edit_action.setEnabled(False)
-mw.connect(edit_action, SIGNAL('triggered()'), JustDoItByYourself)
+aqt.mw.connect(edit_action, SIGNAL('triggered()'), JustDoItByYourself)
 
 
-images_action = QAction(MSG[lang]['Edit Images'], mw)
+images_action = QAction(MSG[lang]['Edit Images'], aqt.mw)
 images_action.setShortcut(QKeySequence(HOTKEY['image']))
 images_action.setEnabled(False)
-mw.connect(images_action, SIGNAL('triggered()'), JustDoItByPictures)
+aqt.mw.connect(images_action, SIGNAL('triggered()'), JustDoItByPictures)
 
 
-sounds_action = QAction(MSG[lang]['Edit Audio'], mw)
+sounds_action = QAction(MSG[lang]['Edit Audio'], aqt.mw)
 sounds_action.setShortcut(QKeySequence(HOTKEY['audio']))
 sounds_action.setEnabled(False)
-mw.connect(sounds_action, SIGNAL('triggered()'), JustDoItBySounds)
+aqt.mw.connect(sounds_action, SIGNAL('triggered()'), JustDoItBySounds)
 
 ##
 
-mw.form.menuEdit.addSeparator()
-mw.form.menuEdit.addAction(edit_action)
-mw.form.menuEdit.addAction(images_action)
-mw.form.menuEdit.addAction(sounds_action)
-mw.form.menuEdit.addSeparator()
+aqt.mw.form.menuEdit.addSeparator()
+aqt.mw.form.menuEdit.addAction(edit_action)
+aqt.mw.form.menuEdit.addAction(images_action)
+aqt.mw.form.menuEdit.addAction(sounds_action)
+aqt.mw.form.menuEdit.addSeparator()
 
 
 def edit_ai_off():
@@ -254,9 +252,9 @@ def edit_ai_on():
     images_action.setEnabled(True)
     sounds_action.setEnabled(True)
 
-mw.deckBrowser.show = wrap(mw.deckBrowser.show, edit_ai_off)
-mw.overview.show = wrap(mw.overview.show, edit_ai_off)
-mw.reviewer.show = wrap(mw.reviewer.show, edit_ai_on)
+aqt.mw.deckBrowser.show = anki.hooks.wrap(aqt.mw.deckBrowser.show, edit_ai_off)
+aqt.mw.overview.show = anki.hooks.wrap(aqt.mw.overview.show, edit_ai_off)
+aqt.mw.reviewer.show = anki.hooks.wrap(aqt.mw.reviewer.show, edit_ai_on)
 
 
 def edit_ai_buttons(editor):
@@ -270,4 +268,48 @@ def edit_ai_buttons(editor):
 # register callback function that gets executed
 #  after setupEditorButtons has run.
 # See Editor.setupEditorButtons for details
-addHook('setupEditorButtons', edit_ai_buttons)
+anki.hooks.addHook('setupEditorButtons', edit_ai_buttons)
+
+## https://ankiweb.net/shared/info/162278717
+
+"""
+Anki Add-on: Refresh Media References
+
+Adds an entry in the Tools menu that clears the webview cache.
+This will effectively refresh all media files used by your cards
+and templates, allowing you to display changes to external files
+without having to restart Anki.
+
+The add-on will also update the modification time of your media
+collection which will force an upload of any updated files
+on the next synchronization with AnkiWeb.
+
+Note: Might lead to increased memory consumption if used excessively
+
+Copyright: (c) Glutanimate 2017
+License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+"""
+
+
+def refresh_media():
+    # clear QWebView cache
+    QWebSettings.clearMemoryCaches()
+    # write a dummy file to update collection.media modtime and force sync
+    media_dir = aqt.mw.col.media.dir()
+    fpath = os.path.join(media_dir, "syncdummy.txt")
+    if not os.path.isfile(fpath):
+        with open(fpath, "w") as f:
+            f.write("anki sync dummy")
+    os.remove(fpath)
+    # reset Anki
+    # aqt.mw.reset()
+    # aqt.utils.tooltip("Media References Updated")
+
+# Set up menus and hooks
+refresh_media_action = QAction("Refresh &Media", aqt.mw)
+refresh_media_action.setShortcut(QKeySequence("Ctrl+Alt+M"))
+refresh_media_action.triggered.connect(refresh_media)
+aqt.mw.form.menuTools.addAction(refresh_media_action)
+
+anki.hooks.addHook("reset", refresh_media)
